7ca54567242767d88545c624054059e02b83780f
Bartłomiej Szkodny <szkobart@outlook.com>
2024-06-17 23:49:18 +0200

    made som comments on level

--- code/level.py
+++ code/level.py
@@ -13,6 +13,8 @@ import re
 import concurrent.futures
 import random
 
+# setting up the level - this is where the player will be spawned, the enemies will be spawned, etc
+
 
 class Level:
     def __init__(self):
@@ -24,11 +26,7 @@ class Level:
         self.enemy_positio = {}
         self.trap_positio = {}
 
-        self.setup()  # setting up the level
-        # setting up the overlay, this is where the player's health will be displayed, etc
-
-        # for cheking if the player is in a new room
-        # make self.plain vector of vectors 5x5
+        self.setup()  # setting up the level - this is where the player will be spawned, the enemies will be spawned, etc
 
         self.previous = [0, 0]
         self.visited_rooms = []
@@ -36,49 +34,52 @@ class Level:
 
         self.list_of_enemies = []
 
+        # setting up the overlay, this is where the player's health will be displayed and camera will be displayed
         self.overlay = overlay(
             self.player, self.visited_rooms, self)
-        # spawn enemies
+
+        # setting up the variables, these variables will be used to control the game
         self.CloseDoors = False
         self.OpenDoors = False
         self.encounter = False
         self.EnterBossRoom = False
-
         self.newFloor = False
-
         self.current_attack = None
+
+        # setting up the groups, these groups will be used to store the sprites
         self.attack_sprites = pygame.sprite.Group()
         self.attackable_sprites = pygame.sprite.Group()
         self.trap_sprites = pygame.sprite.Group()
 
     def setup(self):  # setting up the level
-        # loading the map from the assets folder in tmx format
 
+        # loading the map from the assets folder in tmx format, i used concurrent.futures to load the map and the level generation at the same time
         with concurrent.futures.ThreadPoolExecutor() as executor:
             future_tmx = executor.submit(
                 load_pygame, '../PythonGame/assets/tileset/map.tmx')
             future_level_gen = executor.submit(LevelGenerationComplete)
 
-    # Retrieve the results once both functions have completed
+            # Retrieve the results once both functions have completed
             self.tmx_data = future_tmx.result()
             self.plain = [[0 for i in range(5)] for j in range(5)]
             self.plain, spawn_i, spawn_j = future_level_gen.result()
 
+        # print(self.plain) how the rooms are connected
         for i in self.plain:
             print("\t".join(map(str, i)))
         for i, _ in enumerate(self.plain):
             for j, Name in enumerate(_):
                 if Name == 0:
-                    Name = '0'
+                    Name = '0'  # 0 is the name of the empty space
                 if Name != '0':
                     for x, y, surf in self.tmx_data.get_layer_by_name(Name).tiles():
-                        Generic(
+                        Generic(  # import all walls
                             ((x % ROOM_WIDTH)*TILE_SIZE+(j-1)*ROOM_WIDTH*TILE_SIZE,
                              (y % ROOM_HEIGHT)*TILE_SIZE+(i-1)*ROOM_HEIGHT*TILE_SIZE),
                             surf,
                             [self.allsprites, self.collision_sprites],
                             LAYERS['walls'])
-                    # import all enemies
+                    # import all enemies and traps
                     if (not re.search("-S", Name) and not re.search("-I", Name)):
                         self.enemy_positio[(i, j)] = {}
                         self.trap_positio[(i, j)] = {}
@@ -93,14 +94,12 @@ class Level:
                                 self.enemy_positio[(i, j)][(
                                     # j to jest nasz y, a i to jest nasz x
                                     obj.x % WIDTH_screen+(j-1)*ROOM_WIDTH*TILE_SIZE, obj.y % HEIGHT_screen+(i-1)*ROOM_HEIGHT*TILE_SIZE)] = obj.name
-                        print(self.trap_positio)
-
-                    # print(self.enemy_positio)
 
         self.player = Player(
             # spawn the player
                             ((spawn_j)*13*64-(64*7), (spawn_i)*9*64-(64*5)), self.allsprites, self.collision_sprites, self.create_attack, self.destroy_attack)
 
+    # check if the player is in a new room, this is used to spawn the enemies, traps and move camera
     def check_if_in_new_room(self, player):
         new_x_y = (player.rect.centery//(9*64)+1,
                    player.rect.centerx//(13*64)+1)
@@ -113,26 +112,26 @@ class Level:
                 pass
             else:
 
-                self.visited_rooms.append([
+                self.visited_rooms.append([  # list of visited rooms for minimap
                     new_x_y[0], new_x_y[1], self.plain[new_x_y[0]][new_x_y[1]]])
                 # print("Visited rooms: ", self.visited_rooms)
                 if ((not re.search("-S", self.plain[new_x_y[0]][new_x_y[1]])) and (not re.search("-I", self.plain[new_x_y[0]][new_x_y[1]])) and (not re.search("-B", self.plain[new_x_y[0]][new_x_y[1]]))):
-                    self.encounter = True
+                    self.encounter = True  # this will spawn the enemies
                 elif (re.search("-B", self.plain[new_x_y[0]][new_x_y[1]])):
-                    self.EnterBossRoom = True
+                    self.EnterBossRoom = True  # this will spawn the boss
 
                 self.previous = new_x_y
 
-    def create_attack(self):
-        # print("Attack")
+    def create_attack(self):  # this is the function for spawning the attack
         self.current_attack = Weapon(
             self.player, [self.allsprites, self.attack_sprites])
 
-    def destroy_attack(self):
+    def destroy_attack(self):  # this is the function for destroying the attack
         if self.current_attack:
             self.current_attack.kill()
         self.current_attack = None
 
+    # this is the function for the player attack logic, like specyfic attack for specyfic enemy
     def player_attack_logic(self):
         if self.attack_sprites:
             for attack_sprite in self.attack_sprites:
@@ -149,11 +148,9 @@ class Level:
 
                         if sprite.health <= 0:
                             sprite.kill()
-                            # print(sprite.health)
-                            print(self.list_of_enemies)
                             loot = random.choice(
+                                # reward for killing enemy
                                 list(sprite.loot_table.keys()))
-                            print(loot)
                             if loot == "money":
                                 self.player.money += sprite.loot_table[loot]
                             else:
@@ -162,19 +159,16 @@ class Level:
                                 else:
                                     INVENTORY_DATA[loot] = sprite.loot_table[loot]
                             if sprite.__class__.__name__ == "Boss":
-                                self.newFloor = True
+                                self.newFloor = True  # if boss is killed, new floor is generated
 
-                            self.list_of_enemies.remove(sprite)
-
+    # this is function that damages player if its sprite colide with trap sprite
     def traps_logic(self):
         for sprite in self.trap_sprites:
             if self.player.hitbox.colliderect(sprite.rect):
                 self.player.health -= 0.1
 
+    # this is the function for spawning the enemies in normal rooms, and spawns traps
     def encounter_spawner(self):
-
-        # make surface in blue that will be displayed in the middle of the room
-
         if self.encounter:
             self.encounter = False
             self.CloseDoors = True
@@ -198,15 +192,10 @@ class Level:
                     groups=[self.allsprites, self.trap_sprites]
                 )
 
-            # print(self.list_of_enemies)
-            # print("Encounter")
-
-    def BossSpawner(self):
+    def BossSpawner(self):  # this is the function for spawning the boss
         if self.EnterBossRoom:
             self.EnterBossRoom = False
             self.CloseDoors = True
-
-            # print("Boss room spawn")
             for position in self.enemy_positio[self.current_room]:
 
                 monster_to_defeat = Boss(
@@ -218,6 +207,7 @@ class Level:
                 )
                 self.list_of_enemies.append(monster_to_defeat)
 
+    # this is the function for damaging the player
     def player_damaged(self, damage):
         self.player.health -= damage
 
@@ -225,7 +215,6 @@ class Level:
 
         if self.CloseDoors:
             if (re.search("U", self.plain[self.current_room[0]][self.current_room[1]])):
-                # print("door up closing")
                 for x, y, surf in self.tmx_data.get_layer_by_name("doors").tiles():
                     Generic(
                         ((45 % ROOM_WIDTH)*TILE_SIZE+(self.player.rect.centerx//(13*64))*ROOM_WIDTH*TILE_SIZE,
